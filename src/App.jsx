ee2661416008fa9cd99672a8173c02643af2beb4
Andre <andregozubuyuk@gmail.com>
2023-09-11 22:38:59 +0300

    Updated files

--- src/App.jsx
+++ src/App.jsx
@@ -13,6 +13,12 @@ import {
   SingleProduct,
 } from "./pages"
 
+import { ErrorElement } from "./components"
+
+//loader
+import { loader as landingLoader } from "./pages/Landing"
+import { loader as singleProductLoader } from "./pages/SingleProduct"
+
 const router = createBrowserRouter([
   {
     path: "/",
@@ -22,6 +28,8 @@ const router = createBrowserRouter([
       {
         index: true,
         element: <Landing />,
+        errorElement: <ErrorElement />,
+        loader: landingLoader,
       },
       {
         path: "products",
@@ -30,6 +38,8 @@ const router = createBrowserRouter([
       {
         path: "products/:id",
         element: <SingleProduct />,
+        errorElement: <ErrorElement />,
+        loader: singleProductLoader,
       },
       {
         path: "cart",
